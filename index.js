ae3a5244bedf04c34f597387558219de9e6efd7b
Ajay <ajay.sheokand@meetribbon.com>
2022-12-18 10:04:16 +0530

    Creating Server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////////////////////
+//Files
 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -7,7 +12,29 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log("File Written");
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data)=>{
-    console.log(data)
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data)=>{
+//     console.log(data)
+// });
+// console.log("Will read file.");
+
+////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res)=>{
+    const pathName = req.url;
+    if(pathName == "/"){
+        res.end('Default Path');
+    }else if(pathName== "/overview"){
+        res.end('This is overview');
+    }else{
+        res.writeHead(404,{
+            "content-type":"text/html",
+            "own-haeder":"own Header"
+        });
+        res.end("<h1>Page not Found</h1>");
+    }
+
 });
-console.log("Will read file.");
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log("Listening to requests on port 8000");
+})
